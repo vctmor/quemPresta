31c62cc05fa6c824ac18b08bc3588774e0c9e3db
victor_moreira <victormoreira@vivaldi.net>
2024-07-27 17:10:55 -0300

    olá Mundão réi sem porteira!

---
+++ src/main/java/arandu/quemPresta/QuemPrestaApplication.java
@@ -0,0 +1,13 @@
+package arandu.quemPresta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuemPrestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuemPrestaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arandu/quemPresta/QuemPrestaApplicationTests.java
@@ -0,0 +1,13 @@
+package arandu.quemPresta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuemPrestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
