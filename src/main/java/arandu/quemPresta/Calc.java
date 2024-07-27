de06e518e55d0156f1c72d0afff15e026df829f7
victor_moreira <victormoreira@vivaldi.net>
2024-07-27 17:29:05 -0300

    teste ci/cd

---
+++ src/main/java/arandu/quemPresta/Calc.java
@@ -0,0 +1,8 @@
+package arandu.quemPresta;
+
+public class Calc {
+    
+    public Double division(double numerator, double denominator)
+
+    {return numerator/denominator;}
+}
---
+++ src/test/java/arandu/quemPresta/CalcTests.java
@@ -0,0 +1,15 @@
+package arandu.quemPresta;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CalcTests {
+    @Test
+    void testDivision() {
+
+        Calc c = new Calc();
+
+        assertEquals(1, c.division(1, 1));
+    }
+}
